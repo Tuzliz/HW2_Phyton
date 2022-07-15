fe4ad0cc4d2045b4e0412b68ab6d011d9d68fd81
liza <lizashatuzhilkina@gmail.com>
2022-07-15 11:42:01 +0300

    ex

--- task4.py
+++ task4.py
@@ -2,7 +2,7 @@
 # не использовать random.randint и вообще библиотеку random
 # Можете использовать xor, биты, библиотеку time (миллисекунды или наносекунды) - для задания случайности
 
-# Случайное число: 
+# Случайное число вариант 1: 
 from time import time 
 num = int(input('Введите число от 1 до 1000000:'))
 x = time()                       #  число из библ time 
@@ -11,6 +11,21 @@ strNum = strNum.replace('.','')
 number = int(strNum)
 rnd_num = number // x 
 print(f'Результат:{rnd_num}')
+
+# вариант 2: 
+from time import time # используем библиотеку time
+def time_random():
+ return time() - float(str(time()).split('.')[0]) 
+print(time()) 
+print(str(time_random()))
+
+def gen_random_range(min, max): # генерируем промежуток выдачи числа 
+ return int(time_random() * (max - min) + min) #умножаем число на разность минимального и иаксимального значения плюс минуты 
+
+if __name__ == '__main__':
+ for i in range(1): # выдаем число из заданного промежутка
+     print (gen_random_range(10,100000))
+
 
 
 
